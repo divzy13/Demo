a43a579597ace7ea530a9e4097a6c6ad3c003514
Raveendran <divya.raveendran@cgi.com>
2019-09-16 13:41:15 +0200

    Add project files.

---
+++ Demo/AutoTest.cs
@@ -0,0 +1,29 @@
+﻿
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class AutoTest
+    {
+        IWebDriver driver;
+
+        
+        [Test]
+        public void Openwindow()
+        {
+            driver = new ChromeDriver("C:\\Users\\divya.raveendran\\Downloads\\chromedriver_win32");
+            driver.Navigate().GoToUrl("https://www.google.co.in/");
+            driver.Manage().Window.Maximize();
+            driver.Quit();
+
+        }
+
+    }
+}

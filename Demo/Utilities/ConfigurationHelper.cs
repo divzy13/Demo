2ed97c015976491a11263130724d5097b322f0b4
divya.raveendran <divya.raveendran@SELI130583>
2019-09-22 00:55:18 +0200

    Enable Chrome fireup

--- Demo/Utilities/ConfigurationHelper.cs
+++ Demo/Utilities/ConfigurationHelper.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Configuration;
-
+using System.Collections.Specialized;
+using System.Reflection;
 
 namespace Demo.Utilities
 {
@@ -10,10 +11,17 @@ namespace Demo.Utilities
     {
         public static T Get<T>(string name)
         {
-            var value = ConfigurationManager.AppSettings[name];
+            ExeConfigurationFileMap map = new ExeConfigurationFileMap();
+            map.ExeConfigFilename = Assembly.GetExecutingAssembly().Location + ".config";
+            Configuration libConfig = ConfigurationManager.OpenMappedExeConfiguration(map, ConfigurationUserLevel.None);
+            AppSettingsSection section = (libConfig.GetSection("appSettings") as AppSettingsSection);
+            Console.WriteLine(section.Settings[name].Value);
+
+
+            var value = section.Settings[name].Value;
             if (typeof(T).IsEnum)
-                return (T)Enum.Parse(typeof(T), value);
-            return (T)Convert.ChangeType(value, typeof(T));
+                    return (T)Enum.Parse(typeof(T), value);
+                return (T)Convert.ChangeType(value, typeof(T));
         }
 
 
